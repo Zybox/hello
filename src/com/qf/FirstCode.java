bce2489aa6cda0489a7b1aaea42a71db70f7af43
Zybox <593018732@qq.com>
2018-07-15 18:42:23 +0800

    Initial commit

---
+++ src/com/qf/FirstCode.java
@@ -0,0 +1,12 @@
+package com.qf;
+
+import java.util.prefs.Preferences;
+
+public class FirstCode {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+}
